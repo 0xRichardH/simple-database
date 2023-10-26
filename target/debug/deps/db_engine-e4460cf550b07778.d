c6613f04f4be7e6bbbadaf10a3a0a836ede8e7f9
Richard Hao <richard@0xdev.dev>
2023-10-26 22:23:24 +0800

    feat: add gitignore

--- target/debug/deps/db_engine-e4460cf550b07778.d
+++
@@ -1,8 +0,0 @@
-/Users/0xdev/workspace/code/simple-database/target/debug/deps/db_engine-e4460cf550b07778.rmeta: db-engine/src/lib.rs Cargo.toml
-
-/Users/0xdev/workspace/code/simple-database/target/debug/deps/db_engine-e4460cf550b07778.d: db-engine/src/lib.rs Cargo.toml
-
-db-engine/src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=-W__CLIPPY_HACKERY__clippy::all__CLIPPY_HACKERY__
--- target/debug/libdb_engine.d
+++
@@ -1,1 +0,0 @@
-/Users/0xdev/workspace/code/simple-database/target/debug/libdb_engine.rlib: /Users/0xdev/workspace/code/simple-database/db-engine/src/lib.rs
